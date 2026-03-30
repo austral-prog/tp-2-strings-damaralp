3e8d1e5eab31bd6afba1f0a7a1e5b23ae4e88c4a
damaralp <damaralpaz@mail.austral.edu.ar>
2026-03-30 12:55:20 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,12 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+
     pass
+
+    first_name = "AdA"
+    last_name = "LoVeLAce"
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.title()} {last_name.title()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"\t{first_name.lower()} {last_name.lower()}")
