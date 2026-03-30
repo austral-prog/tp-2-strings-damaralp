d00ca81b2a2d707884294c8324c7280efece389e
damaralp <damaralpaz@mail.austral.edu.ar>
2026-03-30 13:14:52 -0300

    Update names.py

--- names.py
+++ names.py
@@ -5,9 +5,11 @@ def names():
 
     pass
 
-    first_name = "AdA"
-    last_name = "LoVeLAce"
-    print(f"{first_name.lower()} {last_name.lower()}")
-    print(f"{first_name.title()} {last_name.title()}")
-    print(f"{first_name.upper()} {last_name.upper()}")
-    print(f"\t{first_name.lower()} {last_name.lower()}")
+    nombre = input()
+    apellido = input()
+    completo = nombre + " " + apellido
+ 
+    print(completo.lower())
+    print(completo.title())
+    print(completo.upper())
+    print("\t" + completo.lower())
